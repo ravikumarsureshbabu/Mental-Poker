54ba67a3fcf2a35c3cdb97b9edbc06670d1888cd
Ravi Kumar Suresh Babu <ravikumar@cs.ucsb.edu>
2016-12-02 13:42:05 -0800

    more info

--- Alice.py
+++ Alice.py
@@ -3,6 +3,12 @@ from crypto import encrypt, decrypt
 
 
 encrypted_card_numbers = []
+
+# connect to BoB
+# get prime from Bob
+# generate encryption key such that gcd(encryptionkey,prime) == 1 
+# generate decryption key such that (encyptionkey)(decyptionkey) = 1 mod(prime)
+
 key = None
 
 for cardnumber in cards.keys():
--- BoB.py
+++ BoB.py
@@ -2,4 +2,10 @@ from pokercards import cards
 from crypto import encrypt, decrypt
 
 
+# connect to ALice
+# generate prime number
+# send it to Alice
+# geberate encyption key
+# generate decyption key 
+
 
