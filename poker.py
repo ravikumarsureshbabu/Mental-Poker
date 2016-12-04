e4da8f523489608700dea5b995c68ca4768e9578
Ravi Kumar Suresh Babu <ravikumar@cs.ucsb.edu>
2016-12-04 00:33:35 -0800

    almost

--- poker.py
+++ poker.py
@@ -8,6 +8,43 @@ class Response:
 		self.amount
 		self.total
 
+def didIwin(me, mycards, othercards, socket, de_key, prime):
+	chance = False
+	o_de_key = None
+	if me == "Bob":
+		chance = True
+	
+	if chance:
+		send(socket,de_key)
+		o_de_key = receive(socket)
+	else
+		o_de_key = receive(socket)
+		send(socket,de_key)
+
+	# decrypt the Bobs cards
+	for index, card in enumerate(othercards):
+		othercards[index] = decrypt(card, o_de_key, prime)
+
+	myscore  = getscore(mycards)
+	otherscore = getscore(othercards)
+	if myscore > otherscore:
+		return True
+	else
+		return False
+
+def getscore(cards_for_score):
+	d = {	"Spades" : 1, 
+			"Hearts" : 2, 
+			"Diamonds" : 3, 
+			"Clubs"  : 4
+			}
+	score = 0
+	for card in cards_for_score:
+		score += cards[card].number
+		score += d[cards[card].suite]
+
+	retrun score
+
 def playmyturn(socket)
 	print("Your Turn!")
 	inp = input("F/f for fold , R/r for raise :")
@@ -35,7 +72,7 @@ def waitforturn(other, socket):
 def printcard(cardnumber)
 	print("Card number :" + cards[cardnumber].number + "Suite :" + cards[cardnumber].suite )
 
-def playPoker(mycards, othercards, me, other, socket):
+def playPoker(mycards, othercards, me, other, socket, de_key, prime):
 	cardnumber = 0
 	print("Your Cards are :")
 	# show first two cards 
@@ -44,7 +81,7 @@ def playPoker(mycards, othercards, me, other, socket):
 	printcard(mycards[cardnumber])
 	cardnumber += 1
 	myturn = True
-	if 0 == random(0,2):
+	if me == "Bob":
 		myturn = False
 	firsttime = True
 	gamestatus = "playing"
@@ -80,7 +117,7 @@ def playPoker(mycards, othercards, me, other, socket):
 		print("You folded, You lose your bet amount of " + myBet)
 	elif gamestatus == "end"
 		print("Time for results !")
-		if didIwin(mycards,othercards,socket):
+		if didIwin(me, mycards,othercards,socket, de_key, prime):
 			print("I won the game and amount of" + otherBet)
 		else
 			print("I lost the game and amount of" + myBet)
